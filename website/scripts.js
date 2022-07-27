79b8c84824465d2849faf52b15e2c313e181fb6f
NatlC <104866249+NatlC@users.noreply.github.com>
2022-07-26 23:53:02 -0400

    Add files via upload

--- website/scripts.js
+++ website/scripts.js
@@ -4,8 +4,13 @@ var modal = document.getElementById('myModal');
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('hallOfFame');
 var img1 = document.getElementById('lifeSaving');
+var img2 = document.getElementById('phcd')
+var img3 = document.getElementById('academicAchievement')
+
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
+
+
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
@@ -16,7 +21,19 @@ img1.onclick = function() {
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
-  }
+}
+
+img2.onclick = function() { 
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+img3.onclick = function() { 
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
